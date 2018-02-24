eca219d68fb77e9cedf7897722d5ff096a1af5d0
Lucas Isoppo <isoppolucas@gmail.com>
2018-02-23 21:27:52 -0300

    Inserindo frontend

---
+++ src/main/webapp/controller/pessoas.js
@@ -0,0 +1,56 @@
+var pessoasModulo = angular.module('pessoasModulo', []);
+
+pessoasModulo.controller("pessoasController", function ($scope, $http) {
+    urlPessoas = 'http://localhost:3000/SistemaImoveis-1.0/rest/pessoas';
+    $scope.listarPessoas = function () {
+        $http.get(urlPessoas).success(function (pessoas) {
+            $scope.pessoas = pessoas;
+        }).error(function (erro) {
+            alert(erro);
+        });
+    }
+    $scope.selecionaPessoas = function (pessoaSelecionada) {
+        $scope.pessoa = pessoaSelecionada;
+    }
+    $scope.limparCampos = function () {
+        $scope.pessoa = "";
+    }
+
+    $scope.salvar = function () {
+        if ($scope.pessoa.codigo == undefined) {
+            console.log('Caiu no POST');
+            $http.post(urlPessoas, $scope.pessoa).success(function (pessoa) {
+                $scope.pessoa.push($scope.pessoa);
+                $scope.listarPessoas();
+                $scope.limparCampos();
+            }).error(function (erro) {
+                alert(erro);
+            });
+
+        } else {
+            console.log('Caiu no PUT');
+            $http.put(urlPessoas, $scope.pessoa).success(function (pessoa) {
+                $scope.listarPessoas();
+                $scope.limparCampos();
+            }).error(function (erro) {
+                alert(erro);
+            });
+        }
+
+    }
+
+    $scope.excluir = function () {
+        if ($scope.pessoa.codigo == undefined) {
+            console.log('Sem pessoa selecionada')
+        } else {
+            $http.delete(urlPessoas+'/'+$scope.pessoa.codigo).success(function(){
+                $scope.listarPessoas();
+                $scope.limparCampos();
+            }).error(function (erro) {
+                alert(erro);
+            });
+        }
+    }
+    //executa
+    $scope.listarPessoas();
+});
